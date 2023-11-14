5cfd9adb526474d17d2770ec033186220e1246d0
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-11-14 20:08:02 +0100

    implementar creacion de las cartas, obtengo la id del drawable de la primera carta, el salto en id es de 11

--- app/src/main/java/com/example/baraja/clases/Baraja.kt
+++ app/src/main/java/com/example/baraja/clases/Baraja.kt
@@ -1,13 +1,26 @@
 package com.example.baraja.clases
 
+import android.util.Range
+
 class Baraja {
 
     val listaCartas = ArrayList<Carta>()
 
-    fun crearBaraja(){
+    fun crearBaraja(primerId:Int){
+        var contador = primerId
         for ((indice,palo) in Palo.values().withIndex())
-            for (nombre in Nombre.values())
-                listaCartas.add(Carta(nombre,palo,indice,if(nombre==Nombre.AS) 11 else indice, 1))
+            for (nombre in Nombre.values()) {
+                listaCartas.add(
+                    Carta(
+                        nombre,
+                        palo,
+                        indice,
+                        if (nombre == Nombre.AS) 11 else indice,
+                        contador
+                    )
+                )
+                contador+=11
+            }
     }
 
     fun barajar(){
