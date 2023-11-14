af6b46b396e6c899c14ec4bb533ca64a286caa3b
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-11-14 20:31:28 +0100

    cambiar cosas de cartas y el main muestra de base la bocaabajo

--- app/src/main/java/com/example/baraja/MainActivity.kt
+++ app/src/main/java/com/example/baraja/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.baraja
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.baraja.clases.Baraja
 import com.example.baraja.ui.theme.BarajaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Baraja.crearBaraja(R.drawable.c01)
+        Baraja.barajar()
         setContent {
             BarajaTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    pantallaBase()
                 }
             }
         }
@@ -30,17 +41,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "${R.drawable.c1},${R.drawable.c10}",
-        modifier = modifier
-    )
-}
+fun pantallaBase(){
+    val contexto = LocalContext.current
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BarajaTheme {
-        Greeting("Android")
+    val cartaActual by rememberSaveable {
+        mutableStateOf(Baraja.dameCarta())
     }
+
+    Column {
+        Image(painter = painterResource(id = R.drawable.c53), contentDescription = "carta del reves")
+    }
+
 }
--- app/src/main/java/com/example/baraja/clases/Baraja.kt
+++ app/src/main/java/com/example/baraja/clases/Baraja.kt
@@ -4,31 +4,37 @@ import android.util.Range
 
 class Baraja {
 
-    val listaCartas = ArrayList<Carta>()
+    companion object{
 
-    fun crearBaraja(){
-        for ((indice,palo) in Palo.values().withIndex())
-            for (nombre in Nombre.values()) {
-                listaCartas.add(
-                    Carta(
-                        nombre,
-                        palo,
-                        indice,
-                        if (nombre == Nombre.AS) 11 else indice,
-                        0
+        val listaCartas = ArrayList<Carta>()
+
+        fun crearBaraja(primerNumero:Int){
+            listaCartas.clear()
+            var contador = primerNumero
+            for ((indice,palo) in Palo.values().withIndex())
+                for (nombre in Nombre.values()) {
+                    listaCartas.add(
+                        Carta(
+                            nombre,
+                            palo,
+                            indice,
+                            if (nombre == Nombre.AS) 11 else indice,
+                            contador
+                        )
                     )
-                )
-            }
-    }
+                    contador++
+                }
+        }
 
-    fun barajar(){
-        listaCartas.shuffle()
-    }
+        fun barajar(){
+            listaCartas.shuffle()
+        }
 
-    fun dameCarta():Carta
-    {
-        val carta = listaCartas.last()
-        listaCartas.remove(carta)
-        return carta
+        fun dameCarta():Carta
+        {
+            val carta = listaCartas.last()
+            listaCartas.remove(carta)
+            return carta
+        }
     }
 }
